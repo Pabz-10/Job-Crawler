118d4e8675dcb07354931255cb57d29cce5a0a42
Pabil <pabil.adhikari10@gmail.com>
2026-01-10 22:39:14 -0800

    PLEASE

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/job-crawler/', // IMPORTANT: Replace 'job-crawler' with your GitHub repository name
+  base: '/Job-Crawler/', // IMPORTANT: This must match the repository name exactly
 })
